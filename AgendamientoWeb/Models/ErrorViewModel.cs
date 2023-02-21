4f4ce8fc6533fae65e1e005b5a57306b7a7f5acd
jbetancurt <jbetancurt@gmail.com>
2023-02-20 21:36:23 -0500

    código inicial

---
+++ AgendamientoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgendamientoWeb.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
